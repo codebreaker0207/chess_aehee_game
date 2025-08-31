f5179fe447e01527569f1363f414d66d1cc0f0d1
codebreaker0207 <bagd57393@gmail.com>
2025-08-31 22:00:52 +0900

    Update firebase-auth.js

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,8 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } 
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } 
   from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
-// Firebase 설정
 const firebaseConfig = {
   apiKey: "AIzaSyDfGn5OYurP78rYRQTBnS_9QOkMCMwQhok",
   authDomain: "aehee-c1b18.firebaseapp.com",
@@ -13,48 +12,32 @@ const firebaseConfig = {
   measurementId: "G-TL6NZ0S78F"
 };
 
-// Firebase 초기화
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
+export let currentUserUid = null;
 
 // 회원가입
-document.getElementById("signupBtn").addEventListener("click", () => {
+document.getElementById("signupBtn")?.addEventListener("click", () => {
   const email = document.getElementById("signup-email").value;
   const password = document.getElementById("signup-password").value;
   createUserWithEmailAndPassword(auth, email, password)
-    .then(userCredential => {
-      alert("회원가입 성공: " + userCredential.user.email);
-    })
-    .catch(error => {
-      alert("회원가입 오류: " + error.message);
-    });
+    .then(() => { alert("회원가입 성공"); })
+    .catch(error => alert("회원가입 오류: " + error.message));
 });
 
 // 로그인
-document.getElementById("loginBtn").addEventListener("click", () => {
+document.getElementById("loginBtn")?.addEventListener("click", () => {
   const email = document.getElementById("login-email").value;
   const password = document.getElementById("login-password").value;
   signInWithEmailAndPassword(auth, email, password)
-    .then(userCredential => {
-      alert("로그인 성공: " + userCredential.user.email);
+    .then(() => { 
+        alert("로그인 성공");
+        location.href = "location/index.html"; // 로그인 후 모둠 페이지로 이동
     })
-    .catch(error => {
-      alert("로그인 오류: " + error.message);
-    });
+    .catch(error => alert("로그인 오류: " + error.message));
 });
 
-// 로그아웃
-document.getElementById("logoutBtn").addEventListener("click", () => {
-  signOut(auth).then(() => {
-    alert("로그아웃 성공");
-  });
-});
-
-// 로그인 상태 표시
+// 로그인 상태 감지
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    document.getElementById("user-info").innerText = "로그인됨: " + user.email;
-  } else {
-    document.getElementById("user-info").innerText = "로그인 안 됨";
-  }
+  currentUserUid = user ? user.uid : null;
 });
